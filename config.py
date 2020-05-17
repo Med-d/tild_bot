9c36b046c864f6a4c8a741f52fa277eb69c88c13
Med-d <meddd.bomj@gmail.com>
2020-05-17 23:10:15 +0500

    Second add

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = TOKEN = "1237210543:AAFEbCD4tI4E9AHJmH1vjIp4FH-exyJCEP8"
 passwordSQL = 'dvuledadvi115'
+admin_id = ''
--- index.py
+++ index.py
@@ -4,24 +4,28 @@ import os
 import shutil
 import pymysql.cursors
 
+#Constants
+SUPER = 'super_user'
+SIMPLE = 'simple_user'
+
 connect = pymysql.connect(host = 'localhost',
                              user = 'root',
                              password = config.passwordSQL,
-                             db = 'active_users',
+                             db = 'users_bot',
                              charset = 'utf8',
                           cursorclass = pymysql.cursors.DictCursor)
 
 def find_user(chat_id, username):
     with connect.cursor() as cursor:
-        cursor.execute("select chat_id from super_users")
+        cursor.execute("select chat_id from perfomer")
         for row in cursor:
             if chat_id == row['chat_id']:
                 return SUPER
-        cursor.execute("select chat_id from simple_users")
+        cursor.execute("select chat_id from customer")
         for row in cursor:
             if chat_id == row['chat_id']:
                 return SIMPLE
-        cursor.execute("insert simple_users(chat_id, name) values ("+str(chat_id)+", '"+str(username)+"');")
+        cursor.execute("insert customer(chat_id, name) values ("+str(chat_id)+", '"+str(username)+"');")
         return SIMPLE
 
 #Connecting to bot
@@ -29,3 +33,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(commands = ['start'])
 def start_dialog(message):
+    if find_user(message.chat.id, message.chat.username) == SUPER:
+        pass
+    else:
+        print('1')
