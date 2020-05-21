f25982a5d0a36871f65dc1e7fef51db2b651b706
Med-d <meddd.bomj@gmail.com>
2020-05-21 11:25:47 +0500

    realise 1.0.4

--- index.py
+++ index.py
@@ -48,7 +48,7 @@ def push_order(short_ord):
     with connect.cursor() as cursor:
         cursor.execute('select chat_id from performer;')
         for row in cursor:
-            if row['chat_id'] != 'NULL':
+            if row['chat_id'].lower() != 'null':
                 bot.send_message(row['chat_id'], 'Новый заказ! \nНазвание: ' + short_ord)
 
 #Начало общения с ботом
